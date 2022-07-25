0981af09fffe6d82cfc5a5479c2940406addad05
unknown <coms@DESKTOP-46NPU1V.dplus.com>
2022-07-25 15:28:28 +0700

    finish register page

--- auth_practice/src/Register.js
+++ auth_practice/src/Register.js
@@ -1,6 +1,35 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+
+  let navigate = useNavigate();
+
+  const signUp = async () => {
+    let path = `../login`;
+
+    try {
+      await axios
+        .post("http://localhost:5000/auth/register", {
+          username: username,
+          user_password: password,
+          first_name: firstName,
+          last_name: lastName,
+        })
+        .then((res) => {
+          alert("Sign up Finished");
+          navigate(path, { replace: true });
+        });
+    } catch (error) {
+      alert(error.errorMsg);
+    }
+  };
+
   return (
     <div className="w-screen h-screen bg-violet-500 ">
       <div
@@ -17,29 +46,38 @@ function Register() {
 
           <input
             type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className="border-2 border-slate-200 rounded-md w-5/6"
           />
           <p className="w-5/6 pb-1 pl-1 pt-5 font-normal">Password</p>
           <input
             type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="border-2 border-slate-200 rounded-md w-5/6"
           />
 
           <p className="w-5/6 pb-1 pl-1 pt-5 font-normal">First name</p>
           <input
             type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
             className="border-2 border-slate-200 rounded-md w-5/6"
           />
 
           <p className="w-5/6 pb-1 pl-1 pt-5 font-normal">Last name</p>
           <input
             type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
             className="border-2 border-slate-200 rounded-md w-5/6"
           />
 
           <input
             type="button"
-            value="Log in"
+            value="Sign up"
+            onClick={signUp}
             className="mt-7 py-1 w-5/6 bg-lime-700 text-white rounded-md"
           />
           <div className="flex mt-5">
